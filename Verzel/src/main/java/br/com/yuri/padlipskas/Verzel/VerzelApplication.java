15c1d57558258140506612470e8762fdfe186e1e
yuriPadlipskas <yuripadlipskas@badgoose.com.br>
2023-10-31 11:28:10 -0300

    Iniciando backend

---
+++ Verzel/src/main/java/br/com/yuri/padlipskas/Verzel/VerzelApplication.java
@@ -0,0 +1,13 @@
+package br.com.yuri.padlipskas.Verzel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerzelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerzelApplication.class, args);
+	}
+
+}
